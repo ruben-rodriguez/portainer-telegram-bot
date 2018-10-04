e18e264cea5bac17cfc6b20f3aec76909f9051f2
ermus19 <ermus19@hotmail.com>
2018-10-04 15:34:28 +0000

    Modify file formatting

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,7 @@
 var assert = require("chai").assert;
 
 describe('Example test', function () {
-
     it('should return -1 when the value is not present', function () {
         assert.equal(-1, [1, 2, 3].indexOf(4));
     });
-
 });
