1211ef588c1789195d8c87a2b9ecee0f34821210
ermus19 <ermus19@hotmail.com>
2018-10-04 15:28:34 +0000

    Add Telegram config parameters:stickers

---
+++ config/telegram-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    stickers:
+                {
+                        ups: 'CAADAQAD-gAD1i-bBkC-JlOab0N8Ag',
+                        yay: 'CAADAQADBgEAAtYvmwYjwIR_8jarRQI',
+                        wat: 'CAADAQADHQEAAtYvmwbA8w6HJ_vVbQI' 
+                },
+    }
