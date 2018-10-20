38c8000a2b5a2e25bc5a0ff90203ecd2a28b8e25
ermus19 <ermus19@hotmail.com>
2018-10-20 17:56:49 +0000

    Add stickers and change parameter order of telegram configuration

--- config/telegram-config.js
+++ config/telegram-config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    stickers:
-                {
-                        ups: 'CAADAQAD-gAD1i-bBkC-JlOab0N8Ag',
-                        yay: 'CAADAQADBgEAAtYvmwYjwIR_8jarRQI',
-                        wat: 'CAADAQADHQEAAtYvmwbA8w6HJ_vVbQI' 
-                },
-    }
+        token: process.env.BOT_TOKEN || 'TEST_TOKEN',
+        users: process.env.TELEGRAM_USERS || '',
+        stickers:
+        {
+                ups: 'CAADAQAD-gAD1i-bBkC-JlOab0N8Ag',
+                yay: 'CAADAQADBgEAAtYvmwYjwIR_8jarRQI',
+                confused: 'CAADAQADHQEAAtYvmwbA8w6HJ_vVbQI',
+                angry: 'CAADAQAD_gAD1i-bBtxkxTuiGVO3Ag',
+                blush: 'CAADAQADBAEAAtYvmwbCtT5Q9o59kAI'
+        }
+}
