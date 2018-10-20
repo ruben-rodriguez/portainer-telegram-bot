c6e2f15f2aeff202b4b6d322e5ee4aec877a8158
ermus19 <ermus19@hotmail.com>
2018-10-20 17:54:26 +0000

    Add Telegram bot event listeners and actions

--- app.js
+++ app.js
@@ -1,23 +1,251 @@
 const logger = require('./config/logger-config');
-const telegramConfig = require('./config/telegram-config');
-const request = require('request');
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
 
-logger.info('Starting Telegram bot controller...');
-const token = process.env.BOT_TOKEN || 'TEST_TOKEN';
+logger.info('Portainer Telegram Bot is up and running!');
 
-logger.info('Using token ' + token);
+const telegramHandler = require('./handlers/telegram-handler');
+const portainerHandler = require('./handlers/portainer-handler');
+
+const token = telegramHandler.getToken();
+
+logger.info('Using Bot token ' + token);
+
 const bot = new Telegraf(token);
 
-bot.on('text', (ctx) => {
-  logger.info('Message received: \'' + ctx.message.text + '\' from ' + ctx.from.first_name);
-  ctx.reply('Message received');
-  ctx.replyWithSticker(telegramConfig.stickers.ups);
+//Retrieve basic information of the Bot
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username;
+  logger.info(botInfo.first_name + ':' + botInfo.username + ' correctly started');
 });
 
-bot.on('sticker', (ctx) => {
-  logger.info('Sticker received: \'' + ctx.message.file_id + '\' from ' + ctx.from.first_name);
-  ctx.replyWithSticker(telegramConfig.stickers.wat);
+//Actions on /hello command
+bot.command('hello', (ctx) => {
+  logger.info('Command /hello from ' + ctx.from.first_name + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+
+
+    ctx.reply('Hello there!\nFor a complete list of available commands, type /help');
+    return ctx.replyWithSticker(telegramHandler.getSticker('blush'));
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+  }
+
 });
 
+//Actions on /help command
+bot.command('help', (ctx) => {
+  logger.info('Command /help from ' + ctx.from.first_name + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+    return ctx.reply('Available commands:\n' +
+      '/help - Prints this list\n' +
+      '/status - Prints Portainer instance status\n' +
+      '/get_containers - Prints list of containers and status\n' +
+      '/start_container - Displays inline keyboard with containers to start if any\n' +
+      '/stop_container - Displays inline keyboard with containers to stop if any'
+    );
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+
+  }
+
+});
+
+//Actions on /status command
+bot.command('status', (ctx) => {
+  logger.info('Command /status from ' + ctx.from.first_name + ':' + ctx.from.username + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+    portainerHandler.isUp().then(function () {
+
+      return ctx.reply('Portainer instance is running');
+
+    }).catch(function (err) {
+
+      return ctx.reply('Portainer instance is not running ' + err);
+
+    });
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+
+  }
+
+});
+
+//Actions on /get_containers command
+bot.command('get_containers', (ctx) => {
+  logger.info('Command /list-containers from ' + ctx.from.first_name + ':' + ctx.from.username + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+    ctx.reply('Retrieving container list... ');
+
+    portainerHandler.getContainers('all').then(function (containers) {
+
+      return ctx.reply(containers);
+
+    }).catch(function (err) {
+
+      return ctx.reply('Could not retrieve container list ' + err);
+
+    });
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+
+  }
+
+});
+
+//Actions on /start_container command
+bot.command('start_container', (ctx) => {
+
+  logger.info('Command /start_container from ' + ctx.from.first_name + ':' + ctx.from.username + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+
+    ctx.reply('Retrieving stopped containers... ');
+
+    portainerHandler.getContainers('stopped').then(function (containers) {
+
+      if (containers.length === 0) {
+
+        ctx.reply('There are no stopped containers to start...');
+
+      } else {
+
+        var keyboard = [];
+        containers.forEach(element => {
+
+          keyboard.push(Markup.callbackButton(element.name, 'start-' + element.name));
+
+          bot.action('start-' + element.name, (ctx) => {
+            ctx.reply('Starting ' + element.name + ' container...');
+
+            portainerHandler.startContainer(element.name).then(function (status) {
+
+              ctx.answerCbQuery();
+              return ctx.reply('Container ' + element.name + ' is now running!');
+
+            }).catch(function (err) {
+
+              ctx.answerCbQuery();
+              return ctx.reply('Unable to start container: ' + err + ' :(');
+
+            });
+
+          })
+        });
+
+        return ctx.reply('Choose a container to start: ',
+          Markup.inlineKeyboard(keyboard)
+            .extra()
+        );
+
+      }
+
+    }).catch(function (err) {
+
+      return ctx.reply('Could not retrieve container list ' + err);
+
+    });
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+
+  }
+
+});
+
+//Actions on /stop_container command
+bot.command('stop_container', (ctx) => {
+
+  logger.info('Command /stop_container from ' + ctx.from.first_name + ':' + ctx.from.username + ' received');
+
+  if (telegramHandler.isAuthorized(ctx.from.username)) {
+
+    ctx.reply('Retrieving running containers... ');
+
+    portainerHandler.getContainers('running').then(function (containers) {
+
+      if (containers.length === 0) {
+
+        ctx.reply('There are no running containers to stop...');
+
+      } else {
+
+        var keyboard = [];
+
+        containers.forEach(element => {
+
+          keyboard.push(Markup.callbackButton(element.name, 'stop-' + element.name));
+
+          bot.action('stop-' + element.name, (ctx) => {
+
+            ctx.reply('Stopping ' + element.name + ' container...');
+
+            portainerHandler.stopContainer(element.name).then(function (status) {
+
+              ctx.answerCbQuery();
+              return ctx.reply(element.name + ' is now stopped');
+
+            }).catch(function (err) {
+
+              ctx.answerCbQuery();
+              return ctx.reply('Unable to stop container: ' + err + ' :(');
+
+            });
+
+          })
+        });
+
+        return ctx.reply('Choose a container to stop: ',
+          Markup.inlineKeyboard(keyboard)
+            .extra()
+        );
+
+      }
+    });
+
+  } else {
+
+    ctx.reply('You are not authorized to use this bot, this incident will be logged and notified');
+    return ctx.replyWithSticker(telegramHandler.getSticker('angry'));
+
+  }
+
+});
+
+//Actions on any other unregistered event
+bot.on('message', (ctx) => {
+  var message = ctx.message.text ||  ctx.message.sticker.file_id;
+  logger.info('Message received: \'' + message + '\' from ' + ctx.from.first_name);
+  return ctx.replyWithSticker(telegramHandler.getSticker('confused'));
+});
+
+//Catch Telegraf bot errors
+bot.catch((err) => {
+  logger.error('Unexpected error: ' + err);
+});
+
+//Start polling Telegram API updates
 bot.startPolling();
