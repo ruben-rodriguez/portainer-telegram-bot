3fbacf610d29f2224b7d42828b377ed6e146ee8b
ermus19 <ermus19@hotmail.com>
2018-10-20 18:02:31 +0000

    Delete app file unit tests

--- test/app.test.js
+++
@@ -1,7 +0,0 @@
-var assert = require("chai").assert;
-
-describe('Example test', function () {
-    it('should return -1 when the value is not present', function () {
-        assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
-});
